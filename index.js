e61ca587d980ce0808c8cbcc972bb1b53d2b8bca
Lewis Horwood <horwood.lewis@gmail.com>
2016-06-01 21:28:55 -0600

    Finished multer and added fun size stats

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express'),
     app = express();
 
-var multer = require('multer');
+var multer = require('multer'),
+    upload = multer();
 
 app.use(express.static(__dirname + '/public'));
 
@@ -10,8 +11,16 @@ app.set('views', './views');
 
 app.get('/', function(req, res){
     res.render('index', {
-        bytes: 'some'
+        bytes: ''
     });
 });
+
+app.post('/', upload.single('doc'), function(req, res){
+    var size = req.file.size;
+    res.render('index', {
+        bytes: size + ' bytes,<br>' + (size/1000).toFixed(2) + ' kilobytes,<br> or ' + (size/1000000).toFixed(2) + ' megabytes.<br><br>' +
+        'That\'s ' + (size/1457664).toFixed(2) + ' floppy disks.'
+    })
+})
 
 app.listen(process.env.PORT || 8080);
